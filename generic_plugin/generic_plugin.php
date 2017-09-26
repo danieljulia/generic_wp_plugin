648192795f5495673f28e6240fefb22cecde9ee8
danieljulia <dani@pimpampum.net>
2017-09-26 15:36:46 +0200

    first commit

---
+++ generic_plugin/generic_plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Plugin Name (change)
+Description: The description for your plugin
+*/
+
+// Register custom post types?
+// https://generatewp.com/post-type/
+
+
+// Register Custom taxonomy?
+// https://generatewp.com/taxonomy/
